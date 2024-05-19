604544ae7eecde0b4aeb1167efdb40e1c7c8dd06
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-05-19 11:32:02 +0300

    final

--- Kapital/app/src/main/java/com/example/kapital/SavingsFragment.java
+++ Kapital/app/src/main/java/com/example/kapital/SavingsFragment.java
@@ -1,6 +1,7 @@
 package com.example.kapital;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +12,11 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -25,19 +26,21 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.DecimalFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class SavingsFragment extends Fragment {
 
     private EditText amountEditText;
     private EditText durationEditText;
-    private AppCompatButton calculateButton;
-    private TextView monthlyBalanceTextView;
+    private Button calculateButton;
+    private TextView dailyBalanceTextView;
     private ProgressBar progressBar;
 
     private double amount;
-    private int durationMonths;
+    private int durationDays;
     private double totalIncome;
     private double totalExpense;
+    private Date startDate;
 
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
@@ -50,8 +53,37 @@ public class SavingsFragment extends Fragment {
         amountEditText = view.findViewById(R.id.amount_edit_text);
         durationEditText = view.findViewById(R.id.duration_edit_text);
         calculateButton = view.findViewById(R.id.calculate_button);
-        monthlyBalanceTextView = view.findViewById(R.id.monthly_balance_text_view);
+        dailyBalanceTextView = view.findViewById(R.id.daily_balance_text_view);
         progressBar = view.findViewById(R.id.progress_bar);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            calculateButton.setEnabled(true);
+        }
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateDailyBalance();
+            }
+        });
+
+        return view;
+    }
+
+    private void calculateDailyBalance() {
+        // Получение введенных пользователем данных
+        try {
+            amount = Double.parseDouble(amountEditText.getText().toString());
+            durationDays = Integer.parseInt(durationEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Установка даты начала на текущую дату
+        startDate = new Date();
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -61,73 +93,48 @@ public class SavingsFragment extends Fragment {
             CollectionReference operationsRef = db.collection("operations");
 
             operationsRef.whereEqualTo("userId", userId)
-                    .whereGreaterThanOrEqualTo("date", getFirstDayOfMonth())
+                    .whereGreaterThanOrEqualTo("date", new Timestamp(startDate))
                     .get()
                     .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                         @Override
                         public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            Log.d("TAGTAG", "найдено");
                             totalIncome = 0;
                             totalExpense = 0;
                             for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
-                                double amount = document.getDouble("amount");
+                                double amount = document.getDouble("sum");
                                 String type = document.getString("type");
-                                if (type != null && type.equals("Доход")) {
+                                if (type != null && type.equals("income")) {
                                     totalIncome += amount;
                                 } else {
                                     totalExpense += amount;
                                 }
                             }
-                            calculateButton.setEnabled(true);
+                            Log.d("TAGTAG", Double.toString(totalIncome));
+
+                            updateUI();
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            calculateButton.setEnabled(false);
-                            // Обработка ошибки получения данных из базы данных
+                            Log.e("TAGTAG", "Error getting documents: ", e);
                         }
                     });
         }
-
-        calculateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculateMonthlyBalance();
-            }
-        });
-
-        return view;
     }
 
-    private void calculateMonthlyBalance() {
-        // Получение введенных пользователем данных
-        try {
-            amount = Double.parseDouble(amountEditText.getText().toString());
-            durationMonths = Integer.parseInt(durationEditText.getText().toString());
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            return;
-        }
+    private void updateUI() {
+        // Рассчет ежедневного баланса
+        double remainingAmount = amount - (totalIncome - totalExpense);
+        double dailyBalance = remainingAmount / durationDays;
 
-        // Рассчет месячного баланса
-        double monthlyBalance = (amount - totalIncome + totalExpense) / durationMonths;
-
-        // Отображение месячного баланса
+        // Отображение ежедневного баланса
         DecimalFormat df = new DecimalFormat("#.##");
-        monthlyBalanceTextView.setText(getString(R.string.monthly_balance, df.format(monthlyBalance)));
+        dailyBalanceTextView.setText(getString(R.string.daily_balance, df.format(dailyBalance)));
 
         // Отображение прогресса
-        double progress = ((amount - totalIncome + totalExpense) / amount) * 100;
-        progressBar.setProgress((int) progress);
-    }
-
-    private Calendar getFirstDayOfMonth() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        return calendar;
+        double progress = ((totalIncome - totalExpense) / amount) * 100;
+        progressBar.setProgress(30);
     }
 }
